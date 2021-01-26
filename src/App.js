e4703ef17d3829e55c068ea6f4210a8c18286918
by09019 <skg09203@naver.com>
2021-01-26 20:30:35 +0900

    #3.2 Planning the Movie Component!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    this.setState(current => ({
+      count : current.count + 1
+    }));
+  };
+  minus = () => {
+    this.setState(current => ({
+      count : current.count - 1
+    }));
+  };
+
+  componentDidMount() { // 처음 렌더링 된 후
+    console.log("I`m Mounted");
+  }
+
+  componentDidUpdate() { // 개발자가 setState 호출 시 render 후 실행
+    console.log("I`m Updating");
+  }
+
+  render () {
+    console.log("I`m rendered");
+    return (
+      <div>
+         <h1>This number is: {this.state.count}</h1>
+         <button onClick={this.add}>Add</button>
+         <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
